8d24256566613e91795a109d722a05e843028345
Gareth Ellis <gareth.ellis0@gmail.com>
2017-07-08 13:22:49 -0700

    added eigen library to better_lane_follow

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+# Installs all the required ros packages
+# (should really be depreceated in favour of using rosdeps properly)
+
+sudo apt-get install -y \
+    libeigen3-dev \
+    ros-kinetic-urg-node
