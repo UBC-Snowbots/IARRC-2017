b89f38301ae1bbde9bc44b511cd96a1f014eae53
Gareth Ellis <gareth.ellis0@gmail.com>
2017-07-08 18:29:50 -0700

    converted line endings to unix, made several minor fixes, and add debug logging

--- src/firmware/firmware/firmware.ino
+++ src/firmware/firmware/firmware.ino
@@ -1,19 +1,34 @@
-/* Firmware for IARRC
+/* Firmware for IARRC 2017
    Author: Vincent Yuan, 
    Modified: Nick Wu
    Modified: James Asefa
    Modified: Jinhao Lu
-   Date Last Modified: June 20 2017
+   Modified: Gareth Ellis
+   Date Last Modified: July 8, 2017
 */
+
+/*
+ * UBC Snowbots - IARRC 2017
+ * Firmware for Control of an RC car
+ * 
+ * This firmware will take in a message of the form:
+ * `B<linear x><linear y><linear z><angular x><angular y><angular z>`
+ * where each `<>` is a single byte. The degree to rotate the servo controlling
+ * the front wheels is determined from `angular z`, and the speed of the car by `linear x`
+ * (the rest of the values are discarded)
+ */
 
 #include <SoftwareSerial.h>
 #include <stdlib.h>
 #include <Servo.h>
 
+// Uncomment this to enable debug logging - messages sent will be echoed back
+//#define DEBUG
+
 #define BAUD_RATE 9600
 
 // size of character buffer being passed over serial connection
-#define BUFFER_SIZE 6
+#define BUFFER_SIZE 7
 
 // Robot speed will be received in a char buffer with each value between 0 and 255
 // as a result, this assigns 128 as stop. Then:
@@ -24,7 +39,7 @@
 #define MAPPED_STRAIGHT_ANGLE 90
 
 // Robot will keep executing the last command unless the period exceeds the INTERVAL
-#define INTERVAL 300
+#define INTERVAL 5000
 
 void serial_read();
 void convert();
@@ -57,6 +72,7 @@ const int ANGULAR_MIN = 35;
 const int ANGULAR_STOP = 90;
 
 unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
 
 Servo motor;
 Servo direction_motor;
@@ -78,9 +94,9 @@ void loop() {
 }
 
 void serial_read(){
-
   // BUFFER_HEAD identifies the start of the buffer
-	if (Serial.read() == BUFFER_HEAD && Serial.available() >= BUFFER_SIZE) {
+	if ( Serial.available() >= BUFFER_SIZE && Serial.read() == BUFFER_HEAD ) {
+    Serial.println("Got message");
 		linear_x = Serial.read();
 		linear_y = Serial.read();
 		linear_z = Serial.read();
@@ -88,18 +104,21 @@ void serial_read(){
 		angular_y = Serial.read();
 		angular_z = Serial.read();
 		previousMillis = currentMillis;
-	} 
-	else {
-		unsigned long currentMillis = millis();
-
-	    if(currentMillis - previousMillis > INTERVAL){
-			linear_x = UNMAPPED_STOP_SPEED;
-			angular_z = MAPPED_STRAIGHT_ANGLE;      
-    	}
+   
+   #ifdef DEBUG
+    Serial.println(linear_x);
+    Serial.println(linear_y);
+    Serial.println(linear_z);
+    Serial.println(angular_x);
+    Serial.println(angular_y);
+    Serial.println(angular_z);
+   #endif
+	} else {
+	  currentMillis = millis();
+    if(currentMillis - previousMillis > INTERVAL){
+	    linear_x = UNMAPPED_STOP_SPEED;
+    }
 	}
-
-	// clear buffer
-	Serial.flush();
 }
 
 void convert() {
